bf41a39d94d0d93db30a6378706f13cea52f4e60
Evangelos Meklis <vmeklis@hotmail.com>
2025-08-04 12:12:14 +0300

    Initial Commit

---
+++ PlannerBoo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlannerBoo
+//
+//  Created by Evangelos Meklis on 4/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlannerBoo/PlannerBooApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannerBooApp.swift
+//  PlannerBoo
+//
+//  Created by Evangelos Meklis on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PlannerBooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
